849d7e4bd78acd88b1e3b70553794b8704e5ed90
Mark Lively <lively@accuweather.com>
2019-12-24 17:37:24 -0500

    Breadth first...

--- 18/main.swift
+++ 18/main.swift
@@ -8,291 +8,257 @@
 
 import Foundation
 
-struct Point:Hashable,Equatable {
-    let x:Int
-    let y:Int
-    var blocked:Set<Point> = []
-    func adjacents() -> Array<Point> {
-        return [Point(x:x+1, y: y, blocked: blocked), Point(x:x-1, y: y, blocked: blocked), Point(x:x, y: y+1, blocked: blocked), Point(x:x, y: y-1, blocked: blocked)]
-    }
-    func blockedAjacents() -> Array<Point> {
-        var bPlus = blocked
-        bPlus.insert(Point(x:x,y:y))
-        return [Point(x:x+1, y: y, blocked: bPlus), Point(x:x-1, y: y, blocked: bPlus), Point(x:x, y: y+1, blocked: bPlus), Point(x:x, y: y-1, blocked: bPlus)]
-    }
 
-    static func == (lhs:Point,rhs:Point) -> Bool{
-        return lhs.x == rhs.x && lhs.y == rhs.y
-    }
-
-    func hash(into hasher: inout Hasher) {
-        hasher.combine(x)
-        hasher.combine(y)
-    }
-
+func adjacents(x:Int,y:Int,blocked:UInt32) -> Array<(Int,Int,UInt32)>{
+    return [(x+1,y,blocked),(x-1,y,blocked),(x,y+1,blocked),(x,y-1,blocked)]
 }
 
-struct ShortCutKey:Equatable,Hashable {
-    let start:Point
-    let remaining:Array<Point>
-}
-
-struct P2P: Equatable, Hashable {
-    let distance:Int
-    let blocking: Set<Point>
-}
-
-typealias Map = Array<Array<Int>>
-typealias ODict = Dictionary<Point,String>
-typealias BlockList = Dictionary<Point,Dictionary<Point,P2P>>
-
-typealias FReachable = (Map,ODict,ODict,Set<Point>,Point) -> Array<(Point,Int)>
-
-var map: Map = []
-var keys: ODict = [:]
-var doors: ODict = [:]
-var start: Point
-
-///WARN this variable is treated as a global.  Best make it into a struct and the have it initialized and send it's reachable function
-var blockList: BlockList = [:]
-
-func buildBlockList(map:Map,keys:ODict,doors:ODict,start:Point) -> BlockList{
-    var out:BlockList = [:]
-    let gridlist = [start] + keys.keys
-    for i in 0 ..< gridlist.count {
-        var nextRow:Dictionary<Point,P2P> = [:]
-        var mapCopy = map
-        var distance = 1
-        var pointlist = gridlist[i].adjacents()
-        mapCopy[gridlist[i].y][gridlist[i].x] = -2
-        repeat {
-            var temp:Array<Point> = []
-            for p in pointlist {
-                if mapCopy[p.y][p.x] == 0 {
-                    mapCopy[p.y][p.x] = distance
-                    if keys[p] != nil {
-                        nextRow[p] = P2P(distance: distance, blocking: p.blocked)
-                        temp.append(contentsOf: p.blockedAjacents())
-                    } else if doors[p] != nil {
-                        temp.append(contentsOf: p.blockedAjacents())
-                    } else {
-                        temp.append(contentsOf: p.adjacents())
-                    }
-                }
-            }
-            distance += 1
-            pointlist = temp
-        } while pointlist.count > 0
-        out[gridlist[i]]=nextRow
-    }
-    return out
-}
-
-func reachablePoints2(_ map:Map ,_ keys:ODict, _ doors:ODict, _ cleared: Set<Point>, _ start:Point) -> Array<(Point,Int)> {
-    var outArray:Array<(Point,Int)> = []
-    let row = blockList[start]!
-    for k in row.keys {
-        if !cleared.contains(k) && row[k]!.blocking.isSubset(of: cleared) {
-            outArray.append((k,row[k]!.distance))
-        }
-    }
-    return outArray
-}
-
-func parseInput(_ input:String)->(Map, ODict, ODict, Point){
-    var keys: ODict = [:]
-    var doors: ODict = [:]
-    var map: Array<Array<Int>> = []
-    var start: Point?
-    var temp: Array<Int> = []
+func parseArray(input:String) -> (Array<Array<Int>>,Array<UInt32>){
+    var distances:Array<Array<Int>> = Array(repeating:Array(repeating: 0, count:27), count:27)
+    var blocking:Array<UInt32> = Array(repeating:0, count:27)
+    var keyX:Array<Int> = Array(repeating: 0, count: 27)
+    var keyY:Array<Int> = Array(repeating: 0, count: 27)
+    var doorX:Array<Int> = Array(repeating: 0, count: 27)
+    var doorY:Array<Int> = Array(repeating: 0, count: 27)
+    var map:Array<Array<Int>> = []
+    var keyCount = 0
     var j = 0
-    for l in input.split(separator: "\n"){
+    for l in input.split(separator: "\n") {
         var i = 0
+        var temp:Array<Int> = []
         for c in l {
             switch c {
             case "#":
-                temp.append(-1)
+                temp.append(-99)
             case ".":
                 temp.append(0)
-            case "a"..."z":
-                keys[Point(x:i,y:j)] = String(c)
-                temp.append(0)
-            case "A"..."Z":
-                doors[Point(x:i,y:j)] = String(c)
-                temp.append(0)
             case "@":
+                keyX[0]=i
+                keyY[0]=j
                 temp.append(0)
-                start = Point(x:i,y:j)
+            case "a"..."z":
+                let n = Int(c.asciiValue! - 96)
+                keyX[n]=i
+                keyY[n]=j
+                temp.append(-n)
+                keyCount += 1
+            case "A"..."Z":
+                let n = Int(c.asciiValue! - 64)
+                doorX[n]=i
+                doorY[n]=j
+                temp.append(-26 - n)
             default:
                 fatalError()
             }
             i += 1
         }
         map.append(temp)
-        temp = []
         j += 1
     }
-    
-    if let start = start {
-        return(map,keys,doors,start)
-    } else {
-        fatalError()
-    }
-}
-
-
-
-func reachableKeys(_ map:Map ,_ keys:ODict, _ doors:ODict, _ cleared: Set<Point>, _ start:Point) -> Array<(Point,Int)> {
-    var reachable:Array<(Point,Int)> = []
-    var mapCopy = map
-    var pointList = start.adjacents()
-    var distance = 1
-    mapCopy[start.y][start.x] = -2
-    repeat {
-        var temp:Array<Point> = []
-        for p in pointList {
-            if mapCopy[p.y][p.x] == 0 {
-                mapCopy[p.y][p.x] = distance
-                if cleared.contains(p) || (doors[p] == nil && keys[p] == nil) {
-                    temp.append(contentsOf:p.adjacents())
-                }
-                if keys[p] != nil && !cleared.contains(p) {
-                    reachable.append((p,distance))
+    let staticMap = map
+    for i in 0...keyCount {
+        map = staticMap
+        map[keyY[i]][keyX[i]] = -99
+        var pointList:Array<(Int,Int,UInt32)> = adjacents(x:keyX[i],y:keyY[i],blocked:UInt32(0))
+        var distance = 1
+        repeat {
+            var temp:Array<(Int,Int,UInt32)> = []
+            for (x,y,blocked) in pointList{
+                switch map[y][x] {
+                case 0:
+                    temp += adjacents(x: x, y: y, blocked: blocked)
+                    map[y][x]=distance
+                case -26 ... -1:
+                    let n = -map[y][x]
+                    map[y][x]=distance
+                    distances[i][n] = distance
+                    if i == 0 {
+                        blocking[n] = blocked
+                    }
+                    temp += adjacents(x: x, y: y, blocked: blocked | 1<<n )
+                case -52 ... -27:
+                    let n = -26 - map[y][x]
+                    map[y][x]=distance
+                    temp += adjacents(x: x, y: y, blocked: blocked | 1<<n )
+                default:
+                    break
                 }
             }
-        }
-        pointList = temp
-        distance += 1
-    } while pointList.count > 0
-    return reachable
+            distance += 1
+            pointList = temp
+        } while pointList.count > 0
+    }
+    return (distances,blocking)
 }
 
-func findShortest(map:Map,keys:ODict,doors:ODict,start:Point, freachable:FReachable = reachableKeys) -> (Array<String>,Int) {
-    var backDoors:Dictionary<String,Point> = [:]
-    for k in doors.keys {
-        backDoors[doors[k]!]=k
+func getAvailable(blocking:Array<UInt32>, found:UInt32, max:Int)->Array<Int> {
+    var out:Array<Int> = []
+    for i in 1...max {
+        if (found | ~blocking[i]) == UInt32.max && ((found >> i) % 2 == 0) {
+            out.append(i)
+        }
     }
-    var foundKeys:Array<String> = Array(repeating: " ", count: keys.count)
-    var bestKeys:Array<String> = Array(repeating: " ", count: keys.count)
-    var minDistance = 999999999
-    var cleared:Set<Point> = []
-    var location = start
+    return out
+}
+
+func solve(distances:Array<Array<Int>>,blocking:Array<UInt32>) -> (Int,String){
+    var keyCount = 0
+    for i in (1...26).reversed(){
+        if distances[0][i] != 0 {
+            keyCount = i
+            break
+        }
+    }
+    var cache:Dictionary<UInt32,Int> = [:]
+    var choices:Array<Int> = Array(repeating:0 , count:keyCount+1)
+    var available:Array<Array<Int>> = Array(repeating:[] , count:keyCount+1)
+    var depth = 0
+    var minDistance = Int.max
+    var bestPath = ""
+    var found:UInt32 = 0
     var distance = 0
-    var choice:Array<Int> = Array(repeating: 0, count: keys.count)
-    var shortCuts:Dictionary<ShortCutKey,(Int,Array<Point>)> = [:]
-    var reachables:Array<Array<(Point,Int)>> = Array(repeating: [], count: keys.count)
-    var done = false
-    var localBest: ShortCutKey?
-    var d = 0
-    var localD = 0
-    repeat {
-        var reachable:Array<(Point,Int)> = []
-        if distance < minDistance {
-            reachable = freachable(map, keys, doors, cleared, location).sorted{$0.1 < $1.1}
-        }
-        if localBest == nil && (reachable.count + d == keys.count) && reachable.count > 1 {
-            localBest = ShortCutKey(start: location, remaining: reachable.map{$0.0})
-            localD = d
-            if let (shortCutDistance, shortCut) = shortCuts[localBest!] {
-                reachable = []
-                distance += shortCutDistance
-                for i in d..<keys.count {
-                    choice[i]=0
-                    foundKeys[i]=keys[shortCut[i-d]]!
+    var path:Array = Array(repeating: 0, count: keyCount+1)
+    for maxDepth in 1...keyCount {
+        print(maxDepth,cache.count)
+        var done = false
+        repeat {
+            let localFound = UInt32(found) + UInt32(path[depth])<<27
+            if depth == maxDepth || distance > minDistance || distance > cache[localFound] ?? .max {
+                if depth == keyCount && distance < minDistance {
+                    minDistance = distance
+                    bestPath = String(bytes: path[1...maxDepth].map{UInt8($0+64)}, encoding: .ascii)!
+                    print(minDistance,bestPath)
                 }
-                d = keys.count - 1
-                var l = location
-                for r in shortCut{
-                    reachables.append([(r,blockList[l]![r]!.distance)])
-                    l = r
+                if depth == maxDepth {
+                    var localFound:UInt32 = UInt32(path[maxDepth])<<27
+                    var dist = 0
+                    var last = 0
+                    for i in 0...keyCount{
+                        dist += distances[last][path[i]]
+                        localFound |= 1<<path[i]
+                        last = path[i]
+                        if cache[localFound] ?? .max > dist {cache[localFound] = dist}
+                    }
                 }
-                print("found shortcut")
-                localBest = nil
-            }
-        }
-        if reachable.count == 0 || distance > minDistance {
-            if let localBest = localBest {
-                var distance = 0
-                var points:Array<Point> = []
-                for i in (choice.count-localBest.remaining.count)..<choice.count {
-                    distance += reachables[i].last!.1
-                    points.append(reachables[i].last!.0)
+                repeat {
+                    distance -= distances[path[depth-1]][path[depth]]
+                    found &= ~(1<<path[depth])
+    //                path[depth]=0
+                    depth -= 1
+                } while  depth > 0 && choices[depth] == available[depth].count - 1
+                if depth == 0 && choices[depth] == available[depth].count - 1 {
+                    done = true
+                } else {
+                    choices[depth] += 1
+                    path[depth+1] = available[depth][choices[depth]]
+                    depth += 1
+                    found |= 1<<path[depth]
+                    distance += distances[Int(path[depth-1])][Int(path[depth])]
                 }
-                if distance < shortCuts[localBest]?.0 ?? distance + 1{
-                    shortCuts[localBest] = (distance,points)
-                }
-            }
-            if distance < minDistance {
-                minDistance = distance
-                bestKeys = foundKeys
-                print(minDistance,bestKeys.joined())
-            }
-            repeat {
-                d -= 1
-                distance -= reachables[d][choice[d]].1
-            } while d > 0 && choice[d] == reachables[d].count-1
-            if d < localD {localBest = nil}
-            if d == 0 {
-                done = true
             } else {
-                distance -= reachables[d][choice[d]].1
-                choice[d] += 1
-                location = reachables[d][choice[d]].0
-                distance += reachables[d][choice[d]].1
-                foundKeys[d] = keys[location]!
+                available[depth] = getAvailable(blocking:blocking, found:found, max:keyCount)
+                choices[depth] = 0
+                path[depth+1] = available[depth][choices[depth]]
+                depth += 1
+                found |= 1<<path[depth]
+                distance += distances[Int(path[depth-1])][Int(path[depth])]
             }
-        } else {
-            choice[d] = 0
-            reachables[d] = reachable
-            distance += reachable[0].1
-            location = reachable[0].0
-            foundKeys[d] = keys[location]!
-            d += 1
+        } while !done
+    }
+    return (minDistance,bestPath)
+}
+extension Array where Element == Int{
+    var key:UInt32 {
+        get {
+            return found + UInt32(self.last!) << 27
         }
-        cleared = []
-        for i in 0 ..< d {
-            cleared.insert(reachables[i][choice[i]].0)
-        }
-        for k in foundKeys {
-            if let p = backDoors[k.uppercased()] {
-                cleared.insert(p)
+    }
+    var found:UInt32 {
+        get {
+            var temp:UInt32 = 0
+            for i in 0..<self.count {
+                temp |= UInt32(1<<self[i])
             }
+            return temp
         }
-    } while !done
-    return (bestKeys,minDistance)
+    }
+}
+struct Path:Hashable,Equatable {
+    let actual:Array<Int>
+    let distance:Int
+    let found:UInt32
+    let key:UInt32
+    init(actual a:Array<Int>,distance d:Int) {
+        actual = a
+        distance = d
+        var temp:UInt32 = 0
+        for i in 0..<a.count {
+            temp |= UInt32(1<<actual[i])
+        }
+        found = temp
+        key = temp + UInt32(a.last!) << 27
+    }
 }
 
-print(1)
-(map,keys,doors,start) = parseInput(test1)
-print(test1a,"---",findShortest(map: map, keys: keys, doors: doors, start: start))
-blockList = buildBlockList(map: map, keys: keys, doors: doors, start: start)
-print(test1a,"___",findShortest(map: map, keys: keys, doors: doors, start: start, freachable: reachablePoints2(_:_:_:_:_:)))
+func solve2(distances:Array<Array<Int>>,blocking:Array<UInt32>) -> (Int,String){
+    var keyCount = 0
+    for i in (1...26).reversed(){
+        if distances[0][i] != 0 {
+            keyCount = i
+            break
+        }
+    }
+    var depth = 0
+    var paths:Array<Path> = [Path(actual: [0], distance: 0)]
+    repeat {
+        var temp:Array<Path> = []
+        for path in paths {
+            var p = path
+            let avail = getAvailable(blocking: blocking, found: path.found, max: keyCount)
+            for a in avail {
+                temp.append(Path(actual: path.actual+[a],distance: path.distance+distances[path.actual.last!][a]))
+            }
+        }
+        if temp.count > 1 {
+            temp.sort{$0.distance < $1.distance}
+            var i = 0
+            var j = 1
+            repeat {
+                j = i + 1
+                repeat {
+                    if temp[i].key == temp[j].key {temp.remove(at: j)}
+                    j += 1
+                } while j < temp.count
+                i += 1
+            } while i < temp.count - 1
+        }
+        depth += 1
+        paths = temp
+        print(depth,paths.count)
+    } while depth < keyCount
+    let (best,path) = (paths[0].distance, paths[0].actual)
+    return (best, String(bytes: path[1...keyCount].map{UInt8($0+64)}, encoding: .ascii)!)
+}
 
-print(2)
-(map,keys,doors,start) = parseInput(test2)
-print(test2a,"---",findShortest(map: map, keys: keys, doors: doors, start: start))
-blockList = buildBlockList(map: map, keys: keys, doors: doors, start: start)
-print(test2a,"___",findShortest(map: map, keys: keys, doors: doors, start: start, freachable: reachablePoints2(_:_:_:_:_:)))
+var (distances,blocking) = parseArray(input: test1)
+print(test1a,solve(distances: distances, blocking: blocking))
+print(test1a,solve2(distances: distances, blocking: blocking))
 
-print(3)
-(map,keys,doors,start) = parseInput(test3)
-print(test3a,"---",findShortest(map: map, keys: keys, doors: doors, start: start))
-blockList = buildBlockList(map: map, keys: keys, doors: doors, start: start)
-print(test3a,"___",findShortest(map: map, keys: keys, doors: doors, start: start, freachable: reachablePoints2(_:_:_:_:_:)))
+(distances,blocking) = parseArray(input: test2)
+print(test2a,solve(distances: distances, blocking: blocking))
+print(test2a,solve2(distances: distances, blocking: blocking))
 
-print(4)
-(map,keys,doors,start) = parseInput(test4)
-//print(test4a,"---",findShortest(map: map, keys: keys, doors: doors, start: start))
-blockList = buildBlockList(map: map, keys: keys, doors: doors, start: start)
-print(test4a,"___",findShortest(map: map, keys: keys, doors: doors, start: start, freachable: reachablePoints2))
+(distances,blocking) = parseArray(input: test3)
+print(test3a,solve2(distances: distances, blocking: blocking))
 
-print(5)
-(map,keys,doors,start) = parseInput(test5)
-print(test5a,"---",findShortest(map: map, keys: keys, doors: doors, start: start))
-blockList = buildBlockList(map: map, keys: keys, doors: doors, start: start)
-print(test5a,"___",findShortest(map: map, keys: keys, doors: doors, start: start, freachable: reachablePoints2))
+(distances,blocking) = parseArray(input: test4)
+print(test4a,solve2(distances: distances, blocking: blocking))
 
+(distances,blocking) = parseArray(input: test5)
+print(test5a,solve2(distances: distances, blocking: blocking))
 
-(map,keys,doors,start) = parseInput(input)
-blockList = buildBlockList(map: map, keys: keys, doors: doors, start: start)
-print("Part 1:",findShortest(map: map, keys: keys, doors: doors, start: start, freachable: reachablePoints2))
+(distances,blocking) = parseArray(input: input)
+print(solve2(distances: distances, blocking: blocking))
+
+//(distances,blocking) = parseArray(input: input2a)
+//print(solve2(distances: distances, blocking: blocking))
